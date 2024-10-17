760127ce509184a4fa8b08b6a025e94593db40d2
Antim Maurya <109455435+Antim21@users.noreply.github.com>
2024-10-17 23:40:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ ReactDOM.render(
       <App/>
     </MyCustomProvider>
   </React.StrictMode>,
-  document.getElementById("root"),
-);
+  document.getElementById("root");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
