aafa409800f00876d5d75a75b235fff5a94ae2e0
Antim Maurya <109455435+Antim21@users.noreply.github.com>
2024-10-29 23:56:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import MyCustomProvider from "./theme/theme";
+import MyCustomProvider from "./theme/theme"
 
 ReactDOM.render(
   <React.StrictMode>
